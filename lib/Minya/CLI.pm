d3d59b585239a34551b46bdc6a7aa1d0514ed7fb
tokuhirom <tokuhirom@gmail.com>
2013-03-20 09:12:56 +0900

    split files

--- lib/Minya/CLI.pm
+++ lib/Minya/CLI.pm
@@ -22,6 +22,7 @@ use Archive::Tar;
 use ExtUtils::MakeMaker qw(prompt);
 use Minya::Metadata;
 use TOML qw(from_toml to_toml);
+use Minya::CLI::New;
 
 use Class::Accessor::Lite 0.05 (
     rw => [qw(minya_toml base_dir work_dir work_dir_base debug config auto_install prereq_specs license)],
@@ -236,118 +237,7 @@ sub register_prereqs {
 # Make new dist
 sub cmd_new {
     my ($self, @args) = @_;
-
-    my $username;
-    my $email;
-    $self->parse_options(
-        \@args,
-        username => \$username,
-        email => \$email,
-    );
-    my $module = shift @args or $self->error("Missing module name\n");
-    $username ||= `git config user.name`;
-    $username =~ s/\n$//;
-    $email ||= `git config user.email`;
-    $email =~ s/\n$//;
-
-    my $version = '0.0.1';
-
-    unless ($username) {
-        $self->error("Please set user.name in git, or use `--username` option.");
-    }
-
-    # $module = "Foo::Bar"
-    # $suffix = "Bar"
-    # $dist   = "Foo-Bar"
-    # $path   = "Foo/Bar.pm"
-    my @pkg    = split /::/, $module;
-    my $suffix = $pkg[ @pkg - 1 ];
-    my $dist   = join "-", @pkg;
-    my $path   = join( "/", @pkg ) . ".pm";
-    ( my $dir = $dist ) =~ s/^App-//;
-    path(path($dist, 'lib', $path)->dirname)->mkpath;
-
-    my $VERSION = '$VERSION';
-    my $author = $username;
-
-    my $module_pm = <<'...';
-package $module;
-use strict;
-use warnings;
-our $VERSION = '$version';
-
-1;
-__END__
-
-=head1 NAME
-
-$module - It's new $module
-
-=head1 SYNOPSIS
-    
-    use $module;
-
-=head1 DESCRIPTION
-
-$module is ...
-
-=head1 LICENSE
-
-Copyright (C) $author
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-$author $<lt> $email E<gt>
-
-...
-    $module_pm =~ s!(\$\w+)!$1!gee;
-    path($dist, 'lib', $path)->spew($module_pm);
-
-    path($dist, '.gitignore')->spew(<<'...');
-/.build/
-/_build/
-/carton.lock
-/.carton/
-/local/
-...
-
-    path( $dist, 'minya.toml' )->spew(
-        to_toml(
-            +{
-                main_module => "lib/$path",
-                "Test::Pod"            => {},
-                "Test::CPANMeta"       => {},
-                "Test::MinimumVersion" => {},
-            }
-        )
-    );
-
-    path($dist, 'cpanfile')->spew(<<'...');
-requires 'perl' => '5.008005';
-
-on test => sub {
-    requires 'Test::More' => 0.58;
-};
-
-on configure => sub {
-    requires 'Module::Build' => 0.40;
-};
-...
-    path($dist, 't')->mkpath;
-    path($dist, 't', '00_compile.t')->spew(sprintf(<<'...', $module));
-use strict;
-use Test::More;
-
-use_ok $_ for qw(
-    %s
-);
-
-done_testing;
-...
-    $self->infof("Finished to create $module\n");
+    $self->Minya::CLI::New::run(@args);
 }
 
 # release to CPAN by CPAN::Uploader
