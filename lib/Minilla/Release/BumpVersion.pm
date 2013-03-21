6e48bf2d16904a22d8f4228ddd289992b13b438c
tokuhirom <tokuhirom@gmail.com>
2013-03-22 07:11:51 +0900

    user can choice how bump up version

--- lib/Minilla/Release/BumpVersion.pm
+++ lib/Minilla/Release/BumpVersion.pm
@@ -2,13 +2,26 @@ package Minilla::Release::BumpVersion;
 use strict;
 use warnings;
 use utf8;
+use ExtUtils::MakeMaker qw(prompt);
 
 sub run {
     my ($self, $c, $opts) = @_;
 
     # perl-revision command is included in Perl::Version.
     if ($opts->{bump}) {
-        $c->cmd('perl-reversion', '-bump');
+        my $answer = prompt("How bump up version? 0: none, 1: major, 2: minor, 3: patch", 3);
+        my @opts;
+        if ($answer =~ /\A[1-3]\z/) {
+            push @opts, +{
+                1 => '-bump-revision',
+                2 => '-bump-version',
+                3 => '-bump-subversion',
+            }->{$answer};
+        }
+        if ($opts->{dry_run}) {
+            push @opts, '-dryrun';
+        }
+        $c->cmd('perl-reversion', @opts);
     } else {
         $c->infof('Skipped bump up');
     }
