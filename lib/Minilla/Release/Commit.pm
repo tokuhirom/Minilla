e40cbbd6879a112a10a9a04a1238c46cbd01159b
tokuhirom <tokuhirom@gmail.com>
2013-03-22 07:40:09 +0900

    Check current repository status before commit.

--- lib/Minilla/Release/Commit.pm
+++ lib/Minilla/Release/Commit.pm
@@ -8,6 +8,11 @@ sub run {
 
     my $ver = $c->config->metadata->version;
 
+    if ( my $unk = `git ls-files -z --others --exclude-standard` ) {
+        $unk =~ s/\0/\n/g;
+        $c->error("Unknown local files:\n$unk\n\nUpdate .gitignore, or git add them\n");
+    }
+
     my $msg = "Checking in changes prior to tagging of version $ver.\n\nChangelog diff is:\n\n";
     $msg .= `git diff Changes`;
 
