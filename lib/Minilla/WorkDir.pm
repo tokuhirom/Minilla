ce7befe140c084e7d3195832c462a65e1ee90d2d
tokuhirom <tokuhirom@gmail.com>
2013-03-22 11:12:06 +0900

    Use Data::Section::Simple for readable code

--- lib/Minilla/WorkDir.pm
+++ lib/Minilla/WorkDir.pm
@@ -8,6 +8,7 @@ use File::pushd;
 use Data::Dumper; # serializer
 use File::Spec::Functions qw(splitdir);
 use File::Basename qw(dirname);
+use Data::Section::Simple qw(get_data_section);
 
 use Minilla::Util qw(randstr);
 use Minilla::CPANMeta;
@@ -185,30 +186,43 @@ sub write_release_tests {
     my $guard = pushd($self->dir);
     path('xt')->mkpath;
 
-    path('xt/minimum_version.t')->spew(<<'...');
+    my $name = $self->c->config->name;
+    for my $file (qw(
+        xt/minimum_version.t
+        xt/cpan_meta.t
+        xt/pod.t
+        xt/spelling.t
+    )) {
+        my $content = get_data_section($file);
+        $content =~s!<<DIST>>!$name!g;
+        path($file)->spew($content);
+    }
+}
+
+1;
+__DATA__
+
+@@ xt/minimum_version.t
 use Test::More;
 eval "use Test::MinimumVersion 0.101080";
 plan skip_all => "Test::MinimumVersion required for testing perl minimum version" if $@;
 all_minimum_version_from_metayml_ok();
-...
 
-    path('xt/cpan_meta.t')->spew(<<'...');
+@@ xt/cpan_meta.t
 use Test::More;
 eval "use Test::CPAN::Meta";
 plan skip_all => "Test::CPAN::Meta required for testing META.yml" if $@;
 plan skip_all => "There is no META.yml" unless -f "META.yml";
 meta_yaml_ok();
-...
 
-    path('xt/pod.t')->spew(<<'...');
+@@ xt/pod.t
 use strict;
 use Test::More;
 eval "use Test::Pod 1.41";
 plan skip_all => "Test::Pod 1.41 required for testing POD" if $@;
 all_pod_files_ok();
-...
 
-    my $spell = <<'...';
+@@ xt/spelling.t
 use strict;
 use Test::More;
 use File::Spec;
@@ -232,11 +246,3 @@ plan skip_all => "no english dict for aspell" unless `aspell dump dicts` =~ /en/
 
 set_spell_cmd('aspell list -l en');
 all_pod_files_spelling_ok('lib');
-...
-
-    $spell=~s!<<DIST>>!$self->c->config->name!ge;
-    path('xt/pod_spell.t')->spew($spell);
-}
-
-1;
-
