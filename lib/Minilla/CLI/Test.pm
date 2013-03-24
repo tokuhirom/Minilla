0dbc5845efd9a88fe7006055afa8d839efb117b1
tokuhirom <tokuhirom@gmail.com>
2013-03-25 06:48:50 +0900

    environment variable management like dzil

--- lib/Minilla/CLI/Test.pm
+++ lib/Minilla/CLI/Test.pm
@@ -10,9 +10,25 @@ use Minilla::Project;
 sub run {
     my ($self, @args) = @_;
 
+    my $release   = 0;
+    my $author    = 1;
+    my $automated = 0;
+    my $all       = 0;
     $self->parse_options(
         \@args,
+        'release!'   => \$release,
+        'author!'    => \$author,
+        'automated!' => \$automated,
+        'all!'       => \$all,
     );
+
+    if ($all) {
+        $release = $author = $automated = 1;
+    }
+
+    $ENV{RELEASE_TESTING}   =1 if $release   == 0;
+    $ENV{AUTHOR_TESTING}    =1 if $author    == 0;
+    $ENV{AUTOMATED_TESTING} =1 if $automated == 0;
 
     my $project = Minilla::Project->new(
         c => $self,
@@ -37,6 +53,13 @@ Minilla::CLI::Test - Run test cases
 =head1 SYNOPSIS
 
     % minil test
+
+        --release      enables the RELEASE_TESTING env variable
+        --automated    enables the AUTOMATED_TESTING env variable
+        --author       enables the AUTHOR_TESTING env variable (default
+                       behavior)
+        --all          enables the RELEASE_TESTING, AUTOMATED_TESTING and
+                       AUTHOR_TESTING env variables
 
 =head1 DESCRIPTION
 
