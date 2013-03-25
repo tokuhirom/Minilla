551eee9e6f992584ed67712ad8255579852c4cba
tokuhirom <tokuhirom@gmail.com>
2013-03-26 06:02:46 +0900

    Show bump version confirmation when there is no tag, too.

--- lib/Minilla/Release/BumpVersion.pm
+++ lib/Minilla/Release/BumpVersion.pm
@@ -16,39 +16,42 @@ sub init {
 sub run {
     my ($self, $project, $opts) = @_;
 
-    my $curver = $project->metadata->version;
-
     # Note: perl-revision command is included in Perl::Version.
-    if (exists_tagged_version($curver)) {
-        my $default_newver = do {
-            my $version = Perl::Version->new( $curver );
-            if ($version->is_alpha) {
-                $version->inc_alpha;
-            } else {
-                my $pos = $version->components-1;
-                $version->increment($pos);
-            }
-            $version;
-        };
-        if (my $ver = prompt("Next Release?", $default_newver)) {
-            my @opts;
-            push @opts, '-set', $ver;
-            if ($opts->{dry_run}) {
-                push @opts, '-dryrun';
-            }
-            unless ($opts->{dry_run}) {
-                cmd('perl-reversion', @opts);
+    if (my $ver = prompt("Next Release?", $self->default_new_version($project))) {
+        my @opts;
+        push @opts, '-set', $ver;
+        if ($opts->{dry_run}) {
+            push @opts, '-dryrun';
+        }
+        unless ($opts->{dry_run}) {
+            cmd('perl-reversion', @opts);
 
-                # clear old version information
-                $project->clear_metadata();
-                my $newver = $project->metadata->version;
-                if (exists_tagged_version($newver)) {
-                    errorf("Sorry, version '%s' is already tagged.  Stopping.\n", $newver);
-                }
+            # clear old version information
+            $project->clear_metadata();
+            my $newver = $project->metadata->version;
+            if (exists_tagged_version($newver)) {
+                errorf("Sorry, version '%s' is already tagged.  Stopping.\n", $newver);
             }
         }
+    }
+}
+
+sub default_new_version {
+    my ($self, $project) = @_;
+    @_==2 or die;
+
+    my $curver = $project->metadata->version;
+    if (not exists_tagged_version($curver)) {
+        $curver;
     } else {
-        infof('Skipped bump up');
+        my $version = Perl::Version->new( $curver );
+        if ($version->is_alpha) {
+            $version->inc_alpha;
+        } else {
+            my $pos = $version->components-1;
+            $version->increment($pos);
+        }
+        $version;
     }
 }
 
