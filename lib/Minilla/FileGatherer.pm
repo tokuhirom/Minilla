3b51885d89a9e53e5f179faeb7e80427d1aa42ef
tokuhirom <tokuhirom@gmail.com>
2013-03-26 05:06:27 +0900

    Use `-z` option for `git ls-files`

--- lib/Minilla/FileGatherer.pm
+++ lib/Minilla/FileGatherer.pm
@@ -8,7 +8,7 @@ use Path::Tiny;
 sub gather_files {
     my ($self, $root) = @_;
     my $guard = pushd($root);
-    my @files = grep { not -l $_ } map { path($_)->relative($root) } split /\n/, `git ls-files`;
+    my @files = grep { not -l $_ } map { path($_)->relative($root) } split /\0/, `git ls-files -z`;
 }
 
 1;
