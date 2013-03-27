8cdf64c557d1bdbe0e7b16ab68d56472704813a2
tokuhirom <tokuhirom@gmail.com>
2013-03-27 10:59:29 +0900

    Suppress warnings on test case.

--- t/migrate/dzil/Acme-Dzil/lib/Acme/Dzil.pm
+++ t/migrate/dzil/Acme-Dzil/lib/Acme/Dzil.pm
@@ -3,3 +3,24 @@ use warnings;
 package Acme::Dzil;
 # ABSTRACT: turns baubles into trinkets
 1;
+__END__
+
+=head1 NAME
+
+Acme::Dzil
+
+=head1 DESCRIPTION
+
+Acme::Foo is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
