97c0e3f49e497130babe44727067c535b6d36f70
tokuhirom <tokuhirom@gmail.com>
2013-03-28 15:46:59 +0900

    Use Module::CPANfile#merge_meta.

--- lib/Minilla/Profile/ModuleBuild.pm
+++ lib/Minilla/Profile/ModuleBuild.pm
@@ -5,6 +5,34 @@ use utf8;
 
 use parent qw(Minilla::Profile::Base);
 
+use File::Spec::Functions qw(catfile);
+use File::Path qw(mkpath);
+use File::Basename qw(dirname);
+use CPAN::Meta;
+use Data::Section::Simple qw(get_data_section);
+use File::pushd;
+
+use Minilla::License::Perl_5;
+
+sub generate {
+    my $self = shift;
+
+    $self->render('Module.pm', catfile('lib', $self->path));
+
+    $self->render('Changes');
+    $self->render('t/00_compile.t');
+    $self->render('.travis.yml');
+
+    $self->render('.gitignore');
+    $self->write_file('LICENSE', Minilla::License::Perl_5->new(
+        holder => sprintf('%s <%s>', $self->author, $self->email)
+    )->fulltext);
+
+    # Generate Build.PL and META.json for installable git repo.
+    $self->render('cpanfile');
+    $self->render('Build.PL');
+}
+
 1;
 __DATA__
 
@@ -14,11 +42,9 @@ use Module::Build;
 use Module::CPANfile;
 use File::Basename;
 use File::Spec;
+use CPAN::Meta;
 
 use 5.008005;
-
-my $cpanfile = Module::CPANfile->load('cpanfile');
-my $prereqs = $cpanfile->prereq_specs;
 
 my $builder = Module::Build->new(
     license              => 'perl',
@@ -26,14 +52,10 @@ my $builder = Module::Build->new(
 
     requires             => {
         perl => '5.008005',
-        %{ $prereqs->{runtime}->{requires} || {} },
     },
     configure_requires => {
-        %{ $prereqs->{runtime}->{requires}  || {}},
-    },
-    build_requires => {
-        %{ $prereqs->{build}->{requires}  || {}},
-        %{ $prereqs->{test}->{requires}   || {}},
+        'Module::Build' => 0.40,
+        'Module::CPANfile' => 0,
     },
 
     no_index    => { 'directory' => [ 'inc' ] },
@@ -49,6 +71,12 @@ my $builder = Module::Build->new(
     create_license => 0,
 );
 $builder->create_build_script();
+
+my $cpanfile = Module::CPANfile->load();
+for my $metafile (grep -e, qw(MYMETA.yml MYMETA.json)) {
+    print "Merging cpanfile prereqs to $metafile\n";
+    $cpanfile->merge_meta($metafile)
+}
 
 @@ cpanfile
 on test => sub {
---
+++ t/profile/module-build.t
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+use t::Util;
+
+use Minilla::Profile::ModuleBuild;
+
+my $guard = pushd(tempdir());
+
+Minilla::Profile::ModuleBuild->new(
+    author => 'hoge',
+    dist => 'Acme-Foo',
+    module => 'Acme::Foo',
+    path => 'Acme/Foo.pm',
+    version => '0.01',
+)->generate();
+
+cmd($^X, 'Build.PL');
+
+like(slurp('MYMETA.json'), qr(Module::CPANfile), 'Module::CPANfile is required');
+like(slurp('MYMETA.yml'), qr(Module::CPANfile), 'Module::CPANfile is required');
+
+done_testing;
+
