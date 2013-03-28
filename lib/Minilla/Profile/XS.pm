89fb177cc98aedbd726ecb2c5732556eac671165
tokuhirom <tokuhirom@gmail.com>
2013-03-28 13:33:36 +0900

    Added ModuleBuild profile.

--- lib/Minilla/Profile/XS.pm
+++ lib/Minilla/Profile/XS.pm
@@ -2,7 +2,7 @@ package Minilla::Profile::XS;
 use strict;
 use warnings;
 use utf8;
-use parent qw(Minilla::Profile::Base);
+use parent qw(Minilla::Profile::ModuleBuild);
 
 use File::Spec::Functions qw(catfile);
 use File::Basename qw(dirname);
@@ -48,61 +48,6 @@ sub generate {
 
 1;
 __DATA__
-
-@@ Build.PL
-use strict;
-use Module::Build;
-use Module::CPANfile;
-use File::Basename;
-use File::Spec;
-
-use 5.008005;
-
-my $cpanfile = Module::CPANfile->load('cpanfile');
-my $prereqs = $cpanfile->prereq_specs;
-
-my $builder = Module::Build->new(
-    license              => 'perl',
-    dynamic_config       => 0,
-
-    requires             => {
-        perl => '5.008005',
-        %{ $prereqs->{runtime}->{requires} || {} },
-    },
-    configure_requires => {
-        %{ $prereqs->{runtime}->{requires}  || {}},
-    },
-    build_requires => {
-        %{ $prereqs->{build}->{requires}  || {}},
-        %{ $prereqs->{test}->{requires}   || {}},
-    },
-
-    no_index    => { 'directory' => [ 'inc' ] },
-    name        => '<% $dist %>',
-    module_name => '<% $module %>',
-
-    script_files => [glob('script/*')],
-
-    test_files           => ((-d '.git' || $ENV{RELEASE_TESTING}) && -d 'xt') ? 't/ xt/' : 't/',
-    recursive_test_files => 1,
-
-    create_readme  => 0,
-    create_license => 0,
-);
-$builder->create_build_script();
-
-@@ cpanfile
-on test => sub {
-    requires 'Test::More' => 0.98;
-};
-
-on configure => sub {
-    requires 'Module::Build' => 0.40;
-    requires 'Module::CPANfile';
-};
-
-on 'develop' => sub {
-};
 
 @@ t/01_simple.t
 use strict;
