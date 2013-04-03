3be2386c470204e283c9cc37d86f758dbe85f25d
tokuhirom <tokuhirom@gmail.com>
2013-04-04 06:48:53 +0900

    Added zsh completion file

---
+++ contrib/zsh-completion.sh
@@ -0,0 +1,11 @@
+_minil() {
+    local -a cmds
+    if (( CURRENT == 2 )); then
+        # \ls lib/Minilla/CLI/*|perl -i -pe 's!.*/!!;s!\.pm!!;tr/A-Z/a-z/'
+        compadd build clean dist help install migrate new release test
+    fi
+
+    return 1;
+}
+
+compdef _minil minil
