45f28c251eaa6924e5c64c2e8dd65e4d2105001d
tokuhirom <tokuhirom@gmail.com>
2013-04-04 15:04:21 +0900

    Checking in changes prior to tagging of version v0.0.50.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1dd0864..feac767 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,19 +2,9 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    -v0.0.49 2013-03-25T09:18:13Z
    +v0.0.50 2013-04-04T06:03:31Z
    
    -
    -    - okay.
    -
    -v0.0.48 2013-03-25T08:05:42Z
    -
    -
    -    - ah.
    -
    -0.0.18
    -
    -0.0.13
    +    - first release on CPAN!
    
     0.0.1 2013-03-18T19:11:49
         - original version

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.0.49");
+use version; our $VERSION = version->declare("v0.0.50");
 
 our $DEBUG;
 our $AUTO_INSTALL;
