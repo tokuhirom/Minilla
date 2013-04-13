b907c6d2279bd31c48b6ea9b51cd4b2431bb5c77
Fuji, Goro <g.psy.va@gmail.com>
2013-04-13 13:32:23 +0900

    show stacktrace for M::E::CommandExit

--- lib/Minilla/Errors.pm
+++ lib/Minilla/Errors.pm
@@ -3,11 +3,21 @@ use strict;
 use warnings;
 use utf8;
 
+use Carp ();
+
 package Minilla::Error::CommandExit;
+
+use overload '""' => 'message', fallback => 1;
+
 sub throw {
     my $class = shift;
-    my $self = bless {}, $class;
+    my $self = bless { message => Carp::longmess($class) }, $class;
     die $self;
+}
+
+sub message {
+    my($self) = @_;
+    return $self->{message};
 }
 
 1;
