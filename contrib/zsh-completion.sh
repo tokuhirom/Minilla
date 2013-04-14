831bedaa905771bcc07b73e8048f464530a686b8
Syohei YOSHIDA <syohex@gmail.com>
2013-04-14 20:43:17 +0900

    Improve zsh completion of minil command

--- contrib/zsh-completion.sh
+++ contrib/zsh-completion.sh
@@ -1,11 +1,69 @@
 _minil() {
-    local -a cmds
-    if (( CURRENT == 2 )); then
-        # \ls lib/Minilla/CLI/*|perl -pe 's!.*/!!;s!\.pm!!;tr/A-Z/a-z/'
-        compadd build clean dist help install migrate new release test
-    fi
+  typeset -A opt_args
+  local context state line
 
-    return 1;
+  local -a _minil_subcommands
+  _minil_subcommands=(
+    'new:Create a new dist'
+    'build:Build distribution'
+    'test:Run test cases'
+    'clean:Clean up directory'
+    'dist:Make dist tarball'
+    'install:Install distribution'
+    'release:Release distribution to CPAN'
+    'migrate:Migrate existed distribution repo'
+    'help:Help me'
+  )
+
+  _arguments '*:: :->subcmd'
+
+  if [[ "$state" == "subcmd" ]];then
+
+    if (( CURRENT == 1 )); then
+      _describe -t commands "minil command" _minil_subcommands -V1
+      return
+    else
+      local opts curcontext
+
+      case "$words[1]" in
+        new)
+          opts=(
+            '(-p|--profile)'{-p,--profile}'[Minilla profile]: :(XS)'
+          )
+          ;;
+        install)
+          opts=('--no-test[Do not run test]')
+          ;;
+        release)
+          opts=(
+            '--no-test[Do not run test]'
+            '--trial[Trial release]'
+            '--dry-test[Dry run mode]'
+          )
+          ;;
+        test)
+          opts=(
+            '--release[enable the RELEASE_TESTING env variable]'
+            '--automated[enable the AUTOMATED_TESTING env variable]'
+            '--author[enable the AUTHOR_TESTING env variable]'
+            '--all[enable the All env variables]'
+          )
+          ;;
+        *)
+          opts=()
+          ;;
+      esac
+      _arguments -s : "$opts[@]" '*::Files:_files'
+    fi
+  fi
 }
 
 compdef _minil minil
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
