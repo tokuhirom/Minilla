5c90057c0937317500152dba32ee25d329f85b43
tokuhirom <tokuhirom@gmail.com>
2013-04-25 15:23:07 +0900

    Most of perl modules does not need configure,build,develop deps.

--- lib/Minilla/Profile/ModuleBuild.pm
+++ lib/Minilla/Profile/ModuleBuild.pm
@@ -35,16 +35,9 @@ sub generate {
 __DATA__
 
 @@ cpanfile
-on 'configure' => sub {
-};
-
-on 'build' => sub {
-};
+requires 'perl', '5.008001';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
-};
-
-on 'develop' => sub {
 };
 
