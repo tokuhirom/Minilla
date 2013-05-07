5702ad041450b4e40019199307e291bfe282ab2b
tokuhirom <tokuhirom@gmail.com>
2013-05-07 14:49:09 +0900

    Cleanup `minil build`. Clean and saner code. Older implementation creates work dir in '.build/XXXXX/' and copy it. New implementation creates work dir in 'Module-Name-X.XX/' directly.

--- lib/Minilla/CLI/Build.pm
+++ lib/Minilla/CLI/Build.pm
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 use utf8;
 
-use Path::Tiny;
-use File::Copy::Recursive qw(rcopy);
+use File::Path qw(rmtree mkpath);
+use File::Spec;
 
 use Minilla::Project;
+use Minilla::WorkDir;
 use Minilla::Logger;
 use Minilla::Util qw(parse_options);
 
@@ -22,15 +23,13 @@ sub run {
     my $project = Minilla::Project->new();
     $project->regenerate_files();
 
-    # generate project directory
-    my $work_dir = $project->work_dir;
-    $work_dir->build();
+    my $dst = File::Spec->rel2abs(sprintf("%s-%s", $project->dist_name, $project->version));
 
-    my $dst = sprintf("%s-%s", $project->dist_name, $project->version);
-    infof("Copying %s to %s\n", $work_dir->dir, $dst);
-    path($dst)->remove_tree();
-    rcopy($work_dir->dir => $dst)
-        or errorf("%s\n", $!);
+    # generate project directory
+    rmtree($dst);
+    mkpath($dst);
+    my $work_dir = Minilla::WorkDir->new(project => $project, dir => $dst);
+    $work_dir->build();
 }
 
 1;
