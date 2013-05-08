e8f1b4004c45a1b4982b0a6914eef12b193ef027
tokuhirom <tokuhirom@gmail.com>
2013-05-08 18:11:16 +0900

    Fixed "Use of uninitialized value $name in substitution (s///) at lib/Minilla/Metadata.pm line 55." error.

--- lib/Minilla/Metadata.pm
+++ lib/Minilla/Metadata.pm
@@ -51,11 +51,13 @@ sub _build_abstract {
     }
     # Parsing pod with Module::Metadata
     {
-        my $name = $self->name || $self->metadata->pod('NAME');
-        $name =~ s/^\s+//gxsm;
-        $name =~ s/\s+$//gxsm;
-        my ($pkg, $abstract) = split /\s+-\s+/, $name, 2;
-        return $abstract if $abstract;
+        my $name = $self->metadata->pod('NAME');
+        if (defined $name) {
+            $name =~ s/^\s+//gxsm;
+            $name =~ s/\s+$//gxsm;
+            my ($pkg, $abstract) = split /\s+-\s+/, $name, 2;
+            return $abstract if $abstract;
+        }
     }
     # find dzil style '# ABSTRACT: '
     {
