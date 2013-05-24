9e90d7af796f4da6dafab6d6e793c6ebccacea40
tokuhirom <tokuhirom@gmail.com>
2013-05-24 13:41:33 +0900

    Checking in changes prior to tagging of version v0.4.4.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 2a37285..eb02849 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v0.4.4 2013-05-24T04:38:41Z
    +
    +    - Depended on latest Pod::Readme
    +      (reported by mlawren++)
    +    - Force name delimiter to be single hyphen.
    +      (moznion++)
    +
     v0.4.3 2013-05-17T07:06:16Z
    
         - Depended on Getopt::Long 2.36 for GetOptionsFromArray

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.4.3");
+use version; our $VERSION = version->declare("v0.4.4");
 
 our $DEBUG;
 our $AUTO_INSTALL;
