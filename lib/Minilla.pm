3ea72ce6e3c34b5054ef8312eb90f64b752e90b8
tokuhirom <tokuhirom@gmail.com>
2013-09-19 13:18:45 +0900

    Checking in changes prior to tagging of version v0.6.7.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index adebafc..e044782 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v0.6.7 2013-09-19T04:16:29Z
    +
    +    - license field in prulal field.
    +      (pointed by leont)
    +
     v0.6.6 2013-09-17T02:47:55Z
    
         - Fixed dependencies

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.6.6");
+use version; our $VERSION = version->declare("v0.6.7");
 
 our $DEBUG;
 our $AUTO_INSTALL;
