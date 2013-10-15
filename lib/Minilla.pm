8852c3c43afeb517931fc12e6f0a585b4bc08ac0
tokuhirom <tokuhirom@gmail.com>
2013-10-15 13:52:32 +0900

    Checking in changes prior to tagging of version v0.8.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 81694f0..9231d3e 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,13 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v0.8.0 2013-10-15T04:48:11Z
    +
    +    - Supports git-submodule
    +      (ysasaki)
    +    - Regenerate Build.PL after
    +      (Shoichi Kaji)
    +
     v0.7.5 2013-10-09T04:05:16Z
    
         - Do not bump up version in share/ directory.

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.7.5");
+use version; our $VERSION = version->declare("v0.8.0");
 
 our $DEBUG;
 our $AUTO_INSTALL;
