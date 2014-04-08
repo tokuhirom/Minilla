087c23b596cf047bdd3c65bfb4e17d5b62eab22a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-04-08 12:13:41 +0900

    Checking in changes prior to tagging of version v0.14.1.
    Changelog diff is:
    
    diff --git Changes Changes
    index 6f9a999..59785ee 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v0.14.1 2014-04-08T03:12:02Z
    +
    +    - Fix case that there is no '.pause'.
    +      (syohex, gcs)
    +
     v0.14.0 2014-04-07T21:15:35Z
    
         - Generate 'authority' section in minil.toml by default.

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v0.14.0");
+use version; our $VERSION = version->declare("v0.14.1");
 
 our $DEBUG;
 our $AUTO_INSTALL;
