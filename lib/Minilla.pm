28536e222c4a29c7bd68f48b9d5b6c9546e91eeb
Syohei YOSHIDA <syohex@gmail.com>
2016-07-28 07:54:01 +0900

    Checking in changes prior to tagging of version v3.0.3.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fc788c8..4b32dc2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v3.0.3 2016-07-27T22:49:54Z
    +
    +    - Update documentation
    +    - Sort keys in Build.PL(#196)
    +    - Fix environment variable issue of test(#195)
    +
     v3.0.2 2016-06-03T23:07:05Z
    
         commit 01fc3652ea8a892dc81ced2fe4534024b8d73777

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.008005;
-use version; our $VERSION = version->declare("v3.0.2");
+use version; our $VERSION = version->declare("v3.0.3");
 
 our $DEBUG;
 our $AUTO_INSTALL;
