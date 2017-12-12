e0e4247eccbd2396a21cc2ee84fa8fd92a6c6080
Shoichi Kaji <skaji@cpan.org>
2017-12-12 22:20:57 +0900

    Checking in changes prior to tagging of version v3.0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0b55fc8..ba8ee37 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Minilla
    
     {{$NEXT}}
    
    +v3.0.14 2017-12-12T13:17:27Z
    +    - Fix an issue where run_tests.t fails on Windows (sago35 #227)
    +
     v3.0.13 2017-06-25T07:08:29Z
         - Handle repository rename on AppVeyor badge (zakame #225)

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.010001;
-use version; our $VERSION = version->declare("v3.0.13");
+use version; our $VERSION = version->declare("v3.0.14");
 
 our $DEBUG;
 our $AUTO_INSTALL;
