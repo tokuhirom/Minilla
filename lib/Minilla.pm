f7daa47cbc9efeefcfe8386ec6388c9882382118
Shoichi Kaji <skaji@cpan.org>
2022-01-31 22:44:34 +0900

    Checking in changes prior to tagging of version v3.1.15.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 74289dc..9e38eff 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Minilla
     
     {{$NEXT}}
     
    +v3.1.15 2022-01-31T13:42:40Z
    +    - Use https for github/gitlab urls (#315)
    +
     v3.1.14 2022-01-07T14:52:37Z
         - Improve deciding default branch(Songmu #313)
         - suppress warnings of redundant argument(Songmu #314)

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.010001;
-use version; our $VERSION = version->declare("v3.1.14");
+use version; our $VERSION = version->declare("v3.1.15");
 
 our $DEBUG;
 our $AUTO_INSTALL;
