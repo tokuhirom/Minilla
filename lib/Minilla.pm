10c81ee0b91f08be9121916db56ba88e39583b35
Shoichi Kaji <skaji@cpan.org>
2022-05-11 05:02:52 +0900

    Checking in changes prior to tagging of version v3.1.18.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b778261..09ba7e7 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Minilla
     
     {{$NEXT}}
     
    +v3.1.18 2022-05-10T20:00:37Z
    +    - Use "prove" in github actions template (#319)
    +
     v3.1.17 2022-05-01T23:20:56Z
         - Change from Travis to GitHub Actions (kfly8 #318)

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.010001;
-use version; our $VERSION = version->declare("v3.1.17");
+use version; our $VERSION = version->declare("v3.1.18");
 
 our $DEBUG;
 our $AUTO_INSTALL;
