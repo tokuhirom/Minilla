e5a0a894e8fa298b7c79184effd0a0c72d676533
Shohei YOSHIDA <syohex@gmail.com>
2023-07-08 14:52:04 +0900

    Checking in changes prior to tagging of version v3.1.22.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 60a1b5e..166abeb 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Minilla
     
     {{$NEXT}}
    +
    +v3.1.22 2023-07-08T05:48:31Z
         - Support package version syntax (#328)
     
     v3.1.21 2023-02-17T14:36:59Z

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.010001;
-use version; our $VERSION = version->declare("v3.1.21");
+use version; our $VERSION = version->declare("v3.1.22");
 
 our $DEBUG;
 our $AUTO_INSTALL;
