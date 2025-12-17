422453438178b3cbbd5a8b9146e586ad6c2e74c5
Shohei YOSHIDA <syohex@gmail.com>
2025-12-18 00:12:17 +0900

    Checking in changes prior to tagging of version v3.1.29.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1cd1cb1..df28a04 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,8 @@
     Revision history for Perl extension Minilla
     
     {{$NEXT}}
    +
    +v3.1.29 2025-12-17T15:08:28Z
         - Fix multibyte characters in POD issue(#340)
     
     v3.1.28 2025-09-15T09:18:56Z

--- lib/Minilla.pm
+++ lib/Minilla.pm
@@ -2,7 +2,7 @@ package Minilla;
 use strict;
 use warnings;
 use 5.010001;
-use version; our $VERSION = version->declare("v3.1.28");
+use version; our $VERSION = version->declare("v3.1.29");
 
 our $DEBUG;
 our $AUTO_INSTALL;
